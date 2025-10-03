d5b6c5db1be47c199832776e152b1ce09392d5f6
Natnael <nattytibe@gmail.com>
2025-10-03 22:36:40 +0300

    db config updated for remote

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,9 +1,9 @@
 const mysql2 = require("mysql2")
 const dbConnection = mysql2.createPool({
-    user: "forum-Admin",
+    user: "E-forumG4",
     database:"evangadi-db",
-    host: "localhost",
-    password:"1234",
+    host: "91.204.209.21",
+    password:"&+cD*Gge{809L(oF",
     connectionLimit: 11
 })
 module.exports= dbConnection.promise()
