2893bdf1015c118fca276ee22b6999f56d51ec42
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-10-03 08:53:10 +0300

    add basic user authentication routes (register, login, checkUser)

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require("express")
 const app = express();
 const port = 3000
 
-const dbConnection = require("./db/dbConfig")
+// const dbConnection = require("./db/dbConfig")
+// middleware for Json parsing 
+app.use(express.json());
+// user routes middleware file
+const userRoutes = require("./routes/userRoute")
 
-const userRoutes = require("./routes/userRoutes")
-app.use("/api/users", userRoutes)
+app.use("/api/user", userRoutes)
 
 async function start(){
-    try{
-        const result = await dbConnection.execute("select, 'test'")    
+    try
+    {
+        // const result = await dbConnection.execute("select, 'test'")    
     app.listen(port)
-    console.log("database connection established");
-    console.log(`listening on ${port}`);
+    // console.log("database connection established");
+    console.log(`Server is running on http://localhost:${port}`);
     }catch(error){
         console.log(error.message);
     }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,22 @@
-// initial test
+const express = require("express");
+const router = express.Router();
+
+// register route
+router.post("/register", (req, res) => {
+  res.send("register user");
+   
+});
+
+router.post("/login", (req, res) => {
+  res.send("login user");
+ 
+});
+
+router.get("/checkUser", (req, res) => {
+  res.send("check user");
+ 
+});
+
+
+
+module.exports = router;
