6b728443d24f56fb9135186024a286b2c3104c2c
Natnael <nattytibe@gmail.com>
2025-10-03 22:39:17 +0300

    db config updated for remote

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,6 +4,7 @@ const dbConnection = mysql2.createPool({
     database:"evangadi-db",
     host: "91.204.209.21",
     password:"&+cD*Gge{809L(oF",
+    port: 3306,
     connectionLimit: 11
 })
 module.exports= dbConnection.promise()
