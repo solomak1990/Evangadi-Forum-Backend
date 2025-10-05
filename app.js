beb3b64ea2f8f26c6561dabe330e866e405d2a8f
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-10-05 09:20:27 +0300

    db,env and app.js is updated

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 3000
+const port = 2025;
+const cors = require("cors");
+app.use(cors());
 
-const dbConnection = require("./db/dbConfig")
+//do connection
+const dbConnection = require("./db/dbConfig");
 
-const userRoutes = require("./routes/userRoutes")
-app.use("/api/users", userRoutes)
+// authotication middlewar
+const authMiddleware = require("./middleware/authMiddleware");
 
-async function start(){
-    try{
-        const result = await dbConnection.execute("select, 'test'")    
-    app.listen(port)
+//user routes middleware file.file.
+const UserRoutes = require("./routes/userRoute");
+
+//do questions middleware
+const questionsRoutes = require("./routes/questionRoute");
+
+const answersRoutes = require("./routes/answerRoute");
+
+//json middleware to extract json
+app.use(express.json());
+
+//user routes middleware
+app.use("/api/users", UserRoutes);
+
+//questions routes middleware
+app.use("/api/questions", authMiddleware, questionsRoutes);
+
+ app.use("/api/questions", questionsRoutes);
+
+//answer routes middleware
+app.use("/api/answers", authMiddleware, answersRoutes);
+ app.use("/api/answers", answersRoutes);
+async function start() {
+  try {
+    const result = await dbConnection.execute("select'test' ");
+    app.listen(port);
     console.log("database connection established");
-    console.log(`listening on ${port}`);
-    }catch(error){
-        console.log(error.message);
-    }
+    console.log(`listening on port  http://localhost:${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-start()
+start();
---
+++ routes/userRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+// register route
+router.post("/register", (req, res) => {
+  res.send("register user");
+});
+
+router.post("/login", (req, res) => {
+  res.send("login user");
+});
+
+router.get("/checkUser", (req, res) => {
+  res.send("check user");
+});
+
+module.exports = router;
