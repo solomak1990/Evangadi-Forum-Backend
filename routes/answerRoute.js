b359c222bf04425fa87c2477172b2e6123246efd
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-10-23 21:15:37 +0300

    delete functionality added

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -6,5 +6,10 @@ const auth = require("../middleware/authMiddleware");
 
 router.get("/:question_id", answerController.getAnswersForQuestion);
 router.post("/", auth, answerController.postAnswer);
+router.put("/:answer_id", auth, answerController.updateAnswer);
+router.delete("/:answer_id", auth, answerController.deleteAnswer);
+
 
 module.exports = router;
+
+
